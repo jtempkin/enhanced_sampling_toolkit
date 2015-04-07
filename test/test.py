97667015474d2e853081989b7f5b68c3b5a7e335
Jeremy Tempkin <jtempkin@uchicago.edu>
2015-04-07 11:57:15 -0500

    Started header for a unit testing capability in ~/test.

---
+++ test/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Head unit testing module for the Enhanced Sampling Toolkit Package.
+
+This file executes a series of Unit Tests written for the various components of the package. Here we use the pytest package. 
+"""
+
+# import header
+import pytest
+
+# now we will proceed through our various module tests. 
