4f5082739f577d7c17efefd50090b4bb56c8573f
Jeremy Tempkin <jtempkin@uchicago.edu>
2015-04-15 17:41:38 -0500

    Added a module for holding observable objects.

---
+++ neus/observables.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains a list of observable routines. These functions serve to calculation and record the value of observables in the course of a calculation.
+
+These routines are initialized as their own class and subsequently can be invoked during other calculations or called by other routines. We may consider updating these classes with a base class prototype similar to how the walker class was initialized. 
+"""
+
