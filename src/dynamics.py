b77194805833878581e1a180d2c928c3fecf10ae
Jeremy Tempkin <jtempkin@uchicago.edu>
2015-08-13 15:51:51 -0500

    added baoab dynamics instance

--- src/dynamics.py
+++ src/dynamics.py
@@ -8,7 +8,7 @@ class langevin():
     This class specifies the langevin dynamcis parameters needed. 
     """
     
-    def __init__(self, temperature, damping_coefficient, shake=False, seed=None):
+    def __init__(self, temperature, damping_coefficient, shake=False, seed=None, linear_momentum=True):
         """
         This constructs a langevin object. Requires that you specify a temperature and a damping coefficient. 
         """
@@ -17,3 +17,20 @@ class langevin():
         self.temperature = temperature
         self.shake = shake
         self.seed = seed
+        self.linear_momentum = linear_momentum
+
+class baoab():
+    """
+    This class implements the baoab integrator.
+    """
+
+    def __init__(self, temperature, damping_coefficient, shake=False, seed=None, linear_momentum=True):
+        """
+        This constructs a langevin object. Requires that you specify a temperature and a damping coefficient.
+        """
+        self.type = "baoab"
+        self.damping_coefficient = damping_coefficient
+        self.temperature = temperature
+        self.shake = shake
+        self.seed = seed
+        self.linear_momentum = linear_momentum
