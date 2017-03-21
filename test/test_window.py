b33f93b2c8ab9ff853c0c72ba05d37846084b980
Jeremy Tempkin <jtempkin@gmail.com>
2017-03-21 11:40:09 -0700

    some edits to test windwo
    Added a few routine headers to be implemented later

--- test/test_window.py
+++ test/test_window.py
@@ -26,6 +26,7 @@ Specifically, this module tests the following functionality:
 import pytest
 import numpy as np
 from est import neus
+from est import entry_points
 
 
 @pytest.fixture
@@ -35,7 +36,22 @@ def setup():
     
     # let's create a window instance
 	window = neus.window.Window([1.0, 1.0], [0.1, 0.1])
-
+	
+	# we're going to add an empty window
+	empty_window = neus.window.Window([1.0, 1.0], [0.1, 0.1])
+	
+	# create an entry point for testing purposes
+	entry = entry_points.entry_point( q=  [1.0, 1.0], 
+                                      p= [1.0, 1.0],
+                                      q_ref= [1.0, 1.0],
+                                      p_ref= [1.0, 1.0],
+                                      time= 0,
+                                      cv= [1.0])
+   
+   # add an entry point to window                    
+   window.add_entry_point()
+                                      
+                                      
 
 def test_initialization():
     """This routine tests that the initialization routine returns a BF object with the correct parameters.s
@@ -56,9 +72,18 @@ def test_fluxlists():
     """Test the behavior of the routines associated with the flux lists.
     """
 
+	# test adding entry points to the flux lists
+	
 
-def test_window_len()
+def test_window_len():
 	"""Test the behavior of the window length function.
 	"""
+	
 
-def test_window_bool()
+def test_window_bool():
+	"""Test the truth value of the window.
+	"""
+
+def test_window_print():
+	"""Test the string __repr__ functionality of the window object
+	"""
